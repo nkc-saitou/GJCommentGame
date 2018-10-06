a03554504b2db9f9613ca7a0a49315f99c27e6b1
DESKTOP-28F38VP\user <nkc.yuki.saitou@gmail.com>
2018-10-06 13:43:30 +0900

    入力システムを実装

---
+++ Assets/Script/WordInput.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordInput : MonoBehaviour {
+
+    InputField inputField;
+
+    /// <summary>
+    /// 入力されたコメントを渡す
+    /// </summary>
+    public string Word { get; private set; }
+
+	// Use this for initialization
+	void Start ()
+    {
+        inputField = FindObjectOfType<InputField>();
+
+        InitInput();
+    }
+
+    /// <summary>
+    /// コメント入力終了
+    /// </summary>
+    public void InputEnd()
+    {
+        Word = inputField.text;
+
+        InitInput();
+
+    }
+
+    /// <summary>
+    /// Input初期化用メソッド
+    /// </summary>
+    void InitInput()
+    {
+        inputField.text = "";
+
+        inputField.ActivateInputField();
+    }
+}
