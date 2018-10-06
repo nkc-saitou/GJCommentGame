5b9382042fe2bc98d294a1d1f0e610c8e0a3477e
Nakamoto <nkc.motoki.nakamura@gmail.com>
2018-10-06 13:39:45 +0900

    文字の出力

---
+++ Assets/Script/WordMover.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordMover : MonoBehaviour
+{
+    const float MOVE_SPEED = 3.0f;
+
+    Transform transformCache;
+    [SerializeField] TextMesh textMeshCache;
+
+    [SerializeField] string testWord;
+    bool isMove = false;
+
+    //=====================================================
+    void Start()
+    {
+        transformCache = transform;
+    }
+    void Update()
+    {
+        Move();
+    }
+    //=====================================================
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    /// <param name="word">表示する文字列</param>
+    public void Initialize(string word)
+    {
+        if (textMeshCache == null) return;
+        textMeshCache.text = word;
+        isMove = true;
+    }
+	
+    /// <summary>
+    /// 移動処理
+    /// </summary>
+    void Move()
+    {
+        if (!isMove) return;
+        transformCache.localPosition += Vector3.left * MOVE_SPEED * Time.deltaTime;
+    }
+	
+    [ContextMenu("TestMove")]
+    void TestMove()
+    {
+        if (testWord == "") return;
+        Initialize(testWord);
+    }
+}
---
+++ Assets/Script/WordOutput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordOutput : MonoBehaviour
+{
+    [SerializeField] WordMover wordPre;
+    [SerializeField] string    outputWord;
+
+    /// <summary>
+    /// 文字オブジェクトの出力
+    /// </summary>
+    /// <param name="word">表示する文字列</param>
+    public void WordShot(string word)
+    {
+        WordMover wordObj = Instantiate(wordPre, new Vector3(12, Random.Range(-4, 4), 0), Quaternion.identity);
+        wordObj.Initialize(word);
+    }
+
+    float RandomHeight(float min, float max)
+    {
+        return Random.Range(min, max);
+    }
+
+    [ContextMenu("出力")]
+    void TestOutput()
+    {
+        WordShot(outputWord);
+    }
+}
